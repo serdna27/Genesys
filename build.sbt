b088743a22be3157ecf5b4eea8b2ea325bc83323
Andres K. Tejada <andres.tejada27@gmail.com>
2015-07-25 19:25:44 -0400

    add the dependencies to sbt

--- build.sbt
+++ build.sbt
@@ -1,6 +1,21 @@
-lazy val root=(project in file(".")).
-   settings(
-	name:="Genesys",
-	version:="0.1",
-	scalaVersion:="2.11.4"
-	)
+name :="Genesys"
+version :="0.1"
+scalaVersion :="2.11.4"
+
+libraryDependencies ++= Seq(
+  // "junit" % "junit" % "4.7" % "test",
+  "org.scala-lang" % "scala-parser-combinators" % "2.11.0-M4",
+  "com.novocode" % "junit-interface" % "0.10" % "test",
+  "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.3",
+  "com.gilt" % "handlebars-scala_2.11" % "2.0.1",
+  "org.json4s" % "json4s-native_2.11" % "3.2.11",
+  "net.sourceforge.jtds" % "jtds" % "1.3.1",
+  "org.xerial" % "sqlite-jdbc" % "3.7.2",
+  "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test",
+  "com.beust" % "jcommander" % "1.30",
+  "com.google.guava" % "guava" % "18.0",
+  "org.postgresql" % "postgresql" % "9.4-1200-jdbc41",
+  "mysql" % "mysql-connector-java" % "5.1.34"
+)
+
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
--- src/main/scala/scala/idx/Genesys/Generator.scala
+++ src/main/scala/scala/idx/Genesys/Generator.scala
@@ -1,7 +1,6 @@
 package scala.idx.Genesys
 
 import com.gilt.handlebars.scala.Handlebars
-import com.gilt.handlebars.scala.helper._
 import scala.xml._
 import scala.idx.Genesys.Domain._
 import com.gilt.handlebars._
@@ -11,7 +10,6 @@ import java.nio.charset.StandardCharsets
 import com.beust.jcommander.JCommander
 import com.google.common
 import com.google.common.base.CaseFormat
-
 object Generator extends App {
 
   case class templateData(val name: String, val label: String, fields: Seq[Map[String, String]])
@@ -20,15 +18,15 @@ object Generator extends App {
     var TemplatesQty: Int = _
     var TemplatesQtyProcessed: Int = _
     private var errorMsgs: List[String] = List.empty[String]
-
-    def errors_=(value: String): Unit = { errorMsgs = errorMsgs.::(value) }
-
-    def errors = errorMsgs
+    
+    def errors_=(value:String):Unit={errorMsgs = errorMsgs.::(value)}
+    
+    def errors=errorMsgs
 
     def addError(errMsg: String) = {
       errorMsgs = errorMsgs.::(errMsg)
     }
-
+    
   }
 
   var config: Configuration = null
@@ -49,52 +47,59 @@ object Generator extends App {
 
     config = Configuration(Args.configFile)
     var entities: List[EntityDef] = List()
-
+    
+    //read the entities based on the source type
     config.sourceType match {
-      case "xml" => entities = (EntityReader(config.source.xml, Args.entities))
-      case "db"  => entities = (EntityReader(config.source.database, Args.entities))
+      case "xml" => entities = (EntityReader(config.source.xml,Args.entities))
+      case "db"  => entities = (EntityReader(config.source.database,Args.entities))
       case _     => throw new Exception("invalid source type")
     }
 
+    //find the format type  used for entities name
     val formatName = config.keysFormat.find { x => x.name == "name" }
 
     try {
-      Result.TemplatesQty = config.templates.size
+      Result.TemplatesQty=config.templates.size
       for (entity <- entities) {
-
-        var entName = entity.name
-        if (!formatName.isEmpty) {
-          formatName.get.format match {
-            case "camelCase"  => entName = toCamelCase(entName)
-            case "pascalCase" => entName = toPascalCase(entName)
-            case _            => throw new Exception("invalid key format")
-          }
-        }
-
+        
         var fields: Seq[Map[String, String]] = Seq.empty
+        
+        //populate the fields found in the entity
         entity.getFields().foreach(f => {
-          f.entName=entName
-          fields = fields.+:(getTemplateField(f))
+          fields=fields.+:(getTemplateField(f,entity.name))
         })
 
         for (template <- config.templates) {
-
           val parse = Handlebars(Configuration.getTemplate(template.content))
-          val filePath = Paths.get(template.setting.directory, Paths.get(template.content).getFileName.toString()).toString()
-
-          saveTemplate(filePath, parse(templateData(entName, entity.label, fields)))
-
-          Result.TemplatesQtyProcessed += 1
+          
+          
+          var entLabel = entity.name
+          var entName=entity.name
+          if (!formatName.isEmpty) {
+            formatName.get.format match {
+              case "camelCase"  => entLabel = toCamelCase(entName)
+              case "pascalCase" => entLabel = toPascalCase(entName)
+              case _            => throw new Exception("invalid key format")
+            }
+          }
+          
+          var fileName=template.setting.prefixName+entLabel+template.setting.suffixName+"."+getFileExtension(template.content)
+          println(s"filename==>${fileName}")
+          val filePath = Paths.get(template.setting.directory,fileName).toString()
+          saveTemplate(filePath, parse(templateData(entName, entLabel, fields)))
+          
+          Result.TemplatesQtyProcessed+=1
         }
 
       }
     } catch {
-      case e: Exception => Result.errors = e.getMessage
-    } finally {
+      case e: Exception => Result.errors=e.getMessage
+    }
+    finally{
       println("Results===>")
-      println("Total Templates:" + Result.TemplatesQty)
-      println("Total Templates Processed:" + Result.TemplatesQtyProcessed)
-      if (Result.errors.size > 0) {
+      println("Total Templates:"+Result.TemplatesQty)
+      println("Total Templates Processed:"+Result.TemplatesQtyProcessed)
+      if(Result.errors.size>0){
         println("Errors===>")
         Result.errors.foreach { x => println(x) }
       }
@@ -102,40 +107,80 @@ object Generator extends App {
 
   }
 
-  private def getTemplateField(f: FieldDef): Map[String, String] = {
+  private def getTemplateField(f: FieldDef,tableName:String): Map[String, String] = {
 
+    //get the mapped type used, i.e database type varchar->string
     val typeMap = if (config.typeMapping.contains(f.typeValue)) config.typeMapping(f.typeValue) else f.typeValue
-
-    var mapDict = Map("isPrimary" -> f.primary.getOrElse(false).toString())
-      .+("isNullable" -> f.nullable.getOrElse(false).toString()).+("length" -> f.length.getOrElse(0).toString())
-      .+("type" -> typeMap).+("type" -> typeMap).+("entName" -> f.entName)
-
+    
+    //map containing the basic information of the column
+    var mapDict = Map("length" -> f.length.getOrElse(0).toString())
+      .+("type"->typeMap).+("columnName"->f.name).+("tableName"->tableName)
+     
+      
+    if(f.nullable.get==true){
+        mapDict=mapDict.+("isNullable"->"true")
+    }
+    if(f.primary.get==true){
+      mapDict=mapDict.+("isPrimary"->"true")
+    }
+    if(f.foreignKey.isDefined){
+      mapDict=mapDict.+("foreignKey"->toPascalCase(f.foreignKey.get))
+    }
+        
+    //get the format for fields name  
     val formatField = config.keysFormat.find { x => x.name == "#fields.name" }
 
     if (!formatField.isEmpty) {
       formatField.get.format match {
-        case "camelCase"  => return (mapDict.+("name" -> toCamelCase(f.name)).+("label" -> toCamelCase(f.name)))
-        case "pascalCase" => return (Map("name" -> toPascalCase(f.name)).+("label" -> toPascalCase(f.name)))
+        case "camelCase"  => return mapDict + ("name" -> toCamelCase(f.name))
+        case "pascalCase" => return mapDict + ("name" -> toPascalCase(f.name))
         case _            => throw new Exception("no key format found")
       }
     } else {
-      return mapDict.+("name" -> f.name).+("label" -> "")
+      return mapDict + ("name" -> f.name)
     }
   }
 
+  def getFileExtension(file:String):String={
+    val dot=file.lastIndexOf(".")
+    return file.substring(dot+1)
+  } 
+  /*
+   public String extension() {
+    int dot = fullPath.lastIndexOf(extensionSeparator);
+    return fullPath.substring(dot + 1);
+  }
+  */
   def toPascalCase(content: String): String = {
     return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, content.replace(" ", "_"));
   }
 
   def toCamelCase(content: String): String = {
-
+    
     return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, content.replace(" ", "_"));
-
+    
   }
 
   private def saveTemplate(file: String, template: String) = {
-    println("template==>" + template)
-    Files.write(Paths.get(file), template.getBytes(StandardCharsets.UTF_8))
+    import java.io.File
+    import java.nio.file._
+    
+    val formmatedTemplate=template.replaceAll("(?m)^[ \t]*\r?\n", "")
+    
+    println("template==>" + formmatedTemplate)
+    var fileInfo=new File(file)
+
+    
+    println("path==>"+fileInfo.getParent())
+    val dirExists=Files.exists(Paths.get(fileInfo.getParent()))
+    //if directory does not exits create it
+    if(dirExists==false)
+    {
+      Files.createDirectories(Paths.get(fileInfo.getParent()))
+    }
+    
+    
+    Files.write(Paths.get(file), formmatedTemplate.getBytes(StandardCharsets.UTF_8))
   }
 
 }
